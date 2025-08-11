46a91f59b2ae24142a5108c8ed96c0e5b0fdc524
Eslaka <Mo0hamed@users.noreply.huggingface.co>
2025-08-11 11:39:26 +0000

    Update app.py
    First version of LLM Chatbot

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import gradio as gr
 from huggingface_hub import InferenceClient
+from transformers import pipeline
 
 """
 For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
 """
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+print("Starting......")
 
 
+chatbot = pipeline("text-generation", model="gpt2")
+
+print("defining function")
 def respond(
     message,
     history: list[tuple[str, str]],
@@ -15,29 +19,14 @@ def respond(
     temperature,
     top_p,
 ):
+    print("enter response")
     messages = [{"role": "system", "content": system_message}]
-
-    for val in history:
-        if val[0]:
-            messages.append({"role": "user", "content": val[0]})
-        if val[1]:
-            messages.append({"role": "assistant", "content": val[1]})
-
     messages.append({"role": "user", "content": message})
-
-    response = ""
-
-    for message in client.chat_completion(
-        messages,
-        max_tokens=max_tokens,
-        stream=True,
-        temperature=temperature,
-        top_p=top_p,
-    ):
-        token = message.choices[0].delta.content
-
-        response += token
-        yield response
+    print("getting response", messages )
+    response = chatbot(messages) 
+    print("got response", response )
+    return response[-1]['generated text'][-1]['content']
+   
 
 
 """
@@ -48,13 +37,8 @@ demo = gr.ChatInterface(
     additional_inputs=[
         gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
+        
+        
         ),
     ],
 )
